d64842cf53c09583c8170c68f3474c96f77d58b3
Fnaticz <oktaviantrisabana@gmail.com>
2025-07-10 05:02:59 +0700

    fix error server part10

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,12 +1,4 @@
-// lib/mongodb.ts
-
 import mongoose, { Mongoose } from 'mongoose';
-
-const MONGO_URI = process.env.MONGO_URI;
-
-if (!MONGO_URI) {
-  throw new Error('MONGO_URI is not defined in environment variables');
-}
 
 interface MongooseCache {
   conn: Mongoose | null;
@@ -32,10 +24,16 @@ if (!cached) {
 }
 
 export async function connectDB(): Promise<Mongoose> {
+  const MONGO_URI = process.env.MONGO_URI;
+
+  if (!MONGO_URI) {
+    throw new Error('MONGO_URI is not defined in environment variables');
+  }
+
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGO_URI!, {
+    cached.promise = mongoose.connect(MONGO_URI, {
       bufferCommands: false,
     });
   }
