9b35e0b5d3823b81b828c014f1ba146903197b9d
Fnaticz <oktaviantrisabana@gmail.com>
2025-07-09 11:10:18 +0700

    Add fix lagiiii v4

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,13 @@
-//src/app/login/page.tsx
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { signIn } from "next-auth/react";
 import Link from 'next/link'
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useRouter } from 'next/navigation';
 import { FcGoogle } from "react-icons/fc";
-import { useEffect } from 'react'
+import Image from 'next/image';
 
 export default function LoginPage() {
   const [formData, setFormData] = useState({ username: '', password: '' });
@@ -38,49 +37,45 @@ export default function LoginPage() {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-  
+
     const res = await fetch('/api/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formData),
     });
-  
+
     const data = await res.json();
     if (!res.ok) {
       setMessage(data.message || "Login failed");
       toast.error(data.message || "Login failed");
       return;
     }
-  
+
     const response = await signIn("credentials", {
       redirect: false,
       username: formData.username,
       password: formData.password,
     });
-  
+
     if (response?.error) {
       toast.error("Invalid username or password");
       setMessage("Invalid username or password");
       return;
     }
-  
-    // buat catch session
+
     try {
       const sessionRes = await fetch('/api/session');
       if (sessionRes.ok) {
         const session = await sessionRes.json();
-  
+
         localStorage.setItem("currentUser", JSON.stringify({
           username: session.username,
           email: session.email,
           role: session.role
         }));
-  
+
         toast.success("Login successful");
-  
-        console.log("Session:", session);
-  
-        // direct sesuai rolee
+
         if (session.role === 'admin') {
           router.push('/dashboard/admin');
         } else if (session.role === 'member') {
@@ -147,7 +142,7 @@ export default function LoginPage() {
             Google
           </button>
           <button className="flex items-center gap-2 px-4 py-2 rounded-full bg-white/10 text-white text-sm hover:bg-white/20 transition">
-            <img src="/facebooklogo.png" className="w-5 h-5" alt="Facebook" />
+            <Image src="/facebooklogo.png" alt="Facebook" width={20} height={20} className="w-5 h-5" />
             Facebook
           </button>
         </div>
@@ -157,5 +152,5 @@ export default function LoginPage() {
         </Link>
       </div>
     </div>
-  )
+  );
 }
