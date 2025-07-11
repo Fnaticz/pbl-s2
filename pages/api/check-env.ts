91f131b50b06e244dc5a19e1f6b9b223d74224bf
Fnaticz <oktaviantrisabana@gmail.com>
2025-07-11 12:51:17 +0700

    delete

--- pages/api/check-env.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(
-  _req: NextApiRequest,
-  res: NextApiResponse
-) {
-  res.json({ MONGO_URI: process.env.MONGO_URI });
-}
-  
