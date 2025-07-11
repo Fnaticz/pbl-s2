5df51388931034b2dac03bbcfdafdabc1f7601bf
Fnaticz <oktaviantrisabana@gmail.com>
2025-07-11 09:24:22 +0700

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import { Poppins } from 'next/font/google'
 import NavbarWrapper from './components/NavbarWrapper'
 import Footer from './components/Footer'
+import SessionWrapper from './components/SessionWrapper'
 
 const font = Poppins({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -17,9 +18,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="id">
       <body className={`${font.className} flex flex-col min-h-screen`}>
-        <NavbarWrapper />
-        <main className="flex-grow">{children}</main>
-        <Footer />
+        <SessionWrapper>
+          <NavbarWrapper />
+          <main className="flex-grow">{children}</main>
+          <Footer />
+        </SessionWrapper>
       </body>
     </html>
   )
