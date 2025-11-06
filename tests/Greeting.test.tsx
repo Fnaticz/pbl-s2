8146b106c69d996ba0ec05994c6a1fd165e363b3
Fnaticz <oktaviantrisabana@gmail.com>
2025-11-07 01:45:05 +0700

    up delete

--- tests/Greeting.test.tsx
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
-import Greeting from '@/app/components/Greeting'
-
-test('menampilkan teks dengan nama yang diberikan', () => {
-  render(<Greeting name="Next.js" />)
-  expect(screen.getByText('Hello, Next.js!')).toBeInTheDocument()
-})
