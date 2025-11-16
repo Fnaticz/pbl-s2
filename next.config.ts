6ebe57cdd5bd321944d4ff7104142cc6383c491f
Fnaticz <oktaviantrisabana@gmail.com>
2025-11-16 22:13:00 +0700

    update image/video

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,16 @@ const nextConfig: NextConfig = {
     MONGO_URI: process.env.MONGO_URI,
   },
   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.firebasestorage.googleapis.com",
+      }
+    ],
     domains: ['res.cloudinary.com', 'pbl-s2.netlify.app', 'localhost'],
   },
   typescript: {
